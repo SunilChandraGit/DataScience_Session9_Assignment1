c9e812d1826c0ce5661bdca9a22ddb09eae1d5f1
neeil93 <chandrasunil93@gmail.com>
2018-05-24 12:34:50 +0530

    Update Problem-4--average of each month.py

--- Problem-4--average of each month.py
+++ Problem-4--average of each month.py
@@ -1,24 +1,27 @@
 import random
 import pandas as pd
 import calendar
+import datetime
 
-max=0
-maxDate='2015-01-01'
-quarter=1
-
-#function to check whether given date is quarters end date or not
-def is_business_quarter_end(date):
-    enddate = pd.to_datetime(date) + pd.DateOffset(days=1)
+#function to check if its months last business day
+def is_business_day_end(date):
     
-    #check if next day is quarter end date and a non-weekday
-    if(date.weekday()==4 and enddate.is_quarter_end and (enddate.weekday()==5 or enddate.weekday()==6)):
+    #check if next day is month end date and a non-weekday
+    enddate = pd.to_datetime(date) + pd.DateOffset(days=1)
+    if(date.weekday()==4 and enddate.is_month_end and (enddate.weekday()==5 or enddate.weekday()==6)):
         return True
     
-    #check if its quarter end
-    if(date.is_quarter_end):
+    #check if its month end day 
+    if(date.is_month_end):
         return True
     
     return False
+
+sum=0
+count=0
+compMonth=1
+
+months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
  
 #get all business days in year 2015
 df = pd.DataFrame({'Date':pd.date_range("2015-01-01", "2015-12-31", freq='B')})
@@ -26,18 +29,20 @@ df = pd.DataFrame({'Date':pd.date_range("2015-01-01", "2015-12-31", freq='B')})
 #generate random number for each business day and add to data frame
 df['Value']=[random.randint(1, 999) for i in range(df['Date'].count())]
 
-#print the header
-print('Quarter\tDate\t\t\tMaxValue\n'+'-'*40)
+print('Month\t\t\tAverage\n'+'-'*30)
 
-#find max in every quarter
+#find sum of values of every month
 for index, rows in df.iterrows():
+    month=rows['Date'].date().month
     
-    if(rows['Value']>max):
-        max=rows['Value']
-        maxDate=rows['Date']
+    sum=sum+rows['Value']
+    count=count+1
     
-    if(is_business_quarter_end(rows['Date'])):
-        print(str(quarter)+'\t'+str(maxDate)+'\t'+str(max))
-        max=0
-        quarter=quarter+1
+    if(is_business_day_end(rows['Date'])):
+        print(months[rows['Date'].date().month-1].ljust(10)+'\t\t'+str(round(sum/count, 2)).ljust(10))
+        sum=0
+        count=0
+        
+
+
     
